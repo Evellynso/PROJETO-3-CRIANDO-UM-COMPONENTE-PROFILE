533688d44f62eb7a54c8b6b816c6b3f8ee34a723
Evellyn lima <159945766+Evellynso@users.noreply.github.com>
2024-11-07 11:31:05 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Profile from './components/Profile'
+
+export default function App() {
+  return( 
+        <div>
+          <Profile 
+           img={'./euu.jfif'}
+           name={'Evellyn Cleyciane'} 
+           bio={'Estudante de Desenvolvimento de Sistemas'} 
+           contact={'+55 (85) 992558499'} 
+           email={'evellynsousa802@gmail.com'} 
+           githubUrl={'https://github.com/Evellynso'} 
+           instagramUrl={'https://www.instagram.com/ellyn_lima'} 
+           />
+        </div> 
+   
+  )
+}
